14a14e0ba3583ac9c62f56c299b92f243fdf0ae8
liumanman <571153769@qq.com>
2018-09-21 16:43:19 +0800

    init

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const state = {
+  count: 2
+}
+const mutations = {
+}
+const actions = {
+
+}
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions
+})
